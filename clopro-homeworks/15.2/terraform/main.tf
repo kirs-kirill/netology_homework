e2fccabeb65ba879db6c4294247acb75a2034cad
Your Name <you@example.com>
2025-06-12 15:14:54 +0000

    update

--- clopro-homeworks/15.2/terraform/main.tf
+++ clopro-homeworks/15.2/terraform/main.tf
@@ -159,6 +159,17 @@ resource "yandex_storage_bucket" "hw_bucket" {
     config_read = true
   }
 }
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = var.service_account_id
+  description        = "static access key for object storage"
+}
+resource "yandex_storage_object" "test-object" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = yandex_storage_bucket.hw_bucket.bucket
+  key        = "1.jpg"
+  source     = "./1.jpg"
+}
 
 resource "yandex_compute_instance_group" "hw-group" {
   folder_id           = var.folder_id
